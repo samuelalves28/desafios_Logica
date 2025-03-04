29786282c740e9d2ea9a8179c753689c60b86fe0
samuelalves28 <83470823+samuelalves28@users.noreply.github.com>
2025-03-04 15:23:54 -0300

    Questão 2

---
+++ 02/Execute02.cs
@@ -0,0 +1,14 @@
+﻿namespace desafios_Logica._02;
+
+public class Execute02
+{
+    public static void Executar()
+    {
+        Console.WriteLine("Informe o raio do circulo: ");
+        var diametro = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+        var area = Math.PI * Math.Pow((diametro / 2), 2);
+
+        Console.WriteLine($"A área do circulo é: {area.ToString("F4")}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using desafios_Logica._01;
+using desafios_Logica._02;
 
 class Program
 {
@@ -7,6 +8,7 @@ class Program
         Console.WriteLine("Escolha a operação: ");
         Console.WriteLine("--------------------");
         Console.WriteLine("1 - Calculo triangulo X e Y.");
+        Console.WriteLine("2 - Calculo diametro do circulo");
         Console.WriteLine("--------------------");
 
         Console.WriteLine("");
@@ -23,6 +25,9 @@ class Program
         {
             case '1':
                 Execute01.Execute();
+                break;
+            case '2':
+                Execute02.Executar();
                 break;
 
         }
