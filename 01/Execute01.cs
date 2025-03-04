5611a3c02fb89561b4ea55ba7d48fddfb3a64445
samuelalves28 <83470823+samuelalves28@users.noreply.github.com>
2025-03-04 15:00:03 -0300

    Adicionar arquivos de projeto.

---
+++ 01/Execute01.cs
@@ -0,0 +1,63 @@
+﻿namespace desafios_Logica._01;
+
+public class Execute01
+{
+    public static void Execute()
+    {
+        var valoresTrianguloX = new List<double>();
+        var valoresTrianguloY = new List<double>();
+
+        ValoresTriangulo(valoresTrianguloX, "X");
+        ValoresTriangulo(valoresTrianguloY, "Y");
+
+        var calculoAreaX = CalculoArea(valoresTrianguloX);
+        var calculoAreaY = CalculoArea(valoresTrianguloY);
+
+        Console.WriteLine($"Area do triangulo X: {calculoAreaX}");
+        Console.WriteLine($"Area do triangulo Y: {calculoAreaY}");
+
+        if (calculoAreaX > calculoAreaY) Console.WriteLine($"O triangulo X é maior que o triangulo Y");
+        else Console.WriteLine($"O triangulo Y é maior que o triangulo X");
+    }
+
+    #region Metodos Privados
+
+    private static void ValoresTriangulo(List<double> valores, string triangulo)
+    {
+        Console.WriteLine($"Digite os valores do triangulo {triangulo}:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("");
+            var valor = Console.ReadLine();
+            valores.Add(Convert.ToDouble(valor.Replace(".", ",")));
+        }
+    }
+
+    private static double CalculoP(List<double> valores)
+    {
+        var A = valores[0];
+        var B = valores[1];
+        var C = valores[2];
+
+        var p = (A + B + C) / 2;
+
+        return p;
+    }
+
+    private static double CalculoArea(List<double> valores)
+    {
+        var A = valores[0];
+        var B = valores[1];
+        var C = valores[2];
+
+        var p = CalculoP(valores);
+
+        var calc = p * (p - A) * (p - B) * (p - C);
+        var area = Math.Sqrt(Convert.ToDouble(calc));
+
+        return area;
+    }
+
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using desafios_Logica._01;
+
+class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Escolha a operação: ");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("1 - Calculo triangulo X e Y.");
+        Console.WriteLine("--------------------");
+
+        Console.WriteLine("");
+        Console.Write("Escolha: ");
+
+
+        var opcao = Console.ReadKey();
+
+        Console.WriteLine("");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("");
+
+        switch (opcao.KeyChar)
+        {
+            case '1':
+                Execute01.Execute();
+                break;
+
+        }
+    }
+}
